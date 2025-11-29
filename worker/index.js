eba0ba0d1ca8298d709e568e94d232c0982701b3
unknown <idcode1690@gmail.com>
2025-11-29 18:20:00 +0900

    chore(pages): deploy build to gh-pages

--- worker/index.js
+++
@@ -1,8 +0,0 @@
-// Deprecated: Worker removed in favor of Cloudflare Pages Functions (`/functions`).
-const removedWorker = {
-  fetch() {
-    return new Response('Worker removed. Use Cloudflare Pages Functions.', { status: 410 });
-  }
-};
-export default removedWorker;
-
